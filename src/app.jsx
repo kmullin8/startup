8121467bcb5b9511b5d6230ae051209c52e379e4
kmullin8 <kadenmullin@gmail.com>
2025-10-08 12:44:13 -0600

    converted play

--- src/app.jsx
+++ src/app.jsx
@@ -40,7 +40,6 @@ export default function App() {
                 </li>
             </menu>
           </nav>
-          <hr/>
         </header>
 
         <Routes>
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import './play.css';
 
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Play displayed here</div>
+    <main>
+      <div
+        className="status-bar"
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: '1em',
+          maxWidth: '700px',
+          margin: 'auto',
+        }}
+      >
+        <div className="players">
+          Player: <span className="player-name">player username</span>
+        </div>
+
+        <div className="score">
+          <label htmlFor="count">Score</label>
+          <input type="text" id="count" defaultValue="--" readOnly />
+        </div>
+
+        <div className="timer">
+          <span>Time Remaining: 0:00</span>
+        </div>
+      </div>
+
+      {/* Question text */}
+      <p
+        className="question-text"
+        style={{ textAlign: 'center', maxWidth: '900px'}}
+      >
+        txt for the question txt for the question txt for the question txt for the question txt
+        for the question txt for the question txt for the question txt for the question
+      </p>
+
+      {/* Answer choices */}
+      <div
+        className="answers"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gap: '1em',
+          justifyContent: 'center',
+          textAlign: 'center',
+          maxWidth: '600px',
+          margin: '2em auto',
+        }}
+      >
+        <button type="submit">Answer A</button>
+        <button type="submit">Answer B</button>
+        <button type="submit">Answer C</button>
+        <button type="submit">Answer D</button>
+      </div>
     </main>
   );
 }
